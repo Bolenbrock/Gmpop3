88909b61fb9568c3be7f6da08880825816f8e57c
Bolenbrock <158115127+Bolenbrock@users.noreply.github.com>
2025-01-30 10:33:26 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,12 @@ const Pop3 = require('node-pop3'); // Используем node-pop3
 
 // Получение токена Telegram и данных для подключения к Gmail из переменных окружения
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
-const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
-const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
+// Заменил на прямые параметры
+// const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
+// const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
+
+const GMAIL_EMAIL = 'maximovfor@gmail.com'; // Укажите свою электронную почту
+const GMAIL_PASSWORD = 'Z12345678u*'; // Укажите свой пароль
 
 // Создаем экземпляр Telegram-бота
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
@@ -14,7 +18,8 @@ const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 // Функция для получения списка писем
 async function getMailList(chatId) {
     console.log(`getMailList: start, chatId: ${chatId}`);
-      console.log(`getMailList: using email: ${GMAIL_EMAIL}, chatId: ${chatId}`);
+    console.log(`getMailList: using email: ${GMAIL_EMAIL}, chatId: ${chatId}`);
+
     // Создаем экземпляр POP3 клиента
     const client = new Pop3({
         host: 'pop.gmail.com',
@@ -23,6 +28,7 @@ async function getMailList(chatId) {
         user: GMAIL_EMAIL,
         password: GMAIL_PASSWORD,
         timeout: 10000, // устанавливаем таймаут 10 секунд
+        debug: true, // Добавляем debug для TLS
     });
     console.log(`getMailList: pop3 client created, chatId: ${chatId}`);
 
@@ -31,18 +37,18 @@ async function getMailList(chatId) {
         console.log(`getMailList: connecting to pop3 server, chatId: ${chatId}`);
         await new Promise((resolve, reject) => {
             client.connect(function (err) {
-                if(err) {
-                     console.error(`getMailList: error connecting to pop3 server, chatId: ${chatId}, error: ${err}`);
-                    reject(err)
+                if (err) {
+                    console.error(`getMailList: error connecting to pop3 server, chatId: ${chatId}, error: ${err}`);
+                    reject(err);
                 }
                 console.log(`getMailList: connected to pop3 server, chatId: ${chatId}`);
-                resolve()
-            })
+                resolve();
+            });
         });
 
-       
+
         // Получаем список сообщений
-          console.log(`getMailList: fetching list of messages, chatId: ${chatId}`);
+        console.log(`getMailList: fetching list of messages, chatId: ${chatId}`);
         const messages = await new Promise((resolve, reject) => {
             client.list(function (err, list) {
                 if (err) {
@@ -57,34 +63,34 @@ async function getMailList(chatId) {
         if (messages.length === 0) {
             bot.sendMessage(chatId, 'Нет новых писем.');
             console.log(`getMailList: no new messages found, chatId: ${chatId}`);
-              await new Promise((resolve, reject) => {
-               client.quit(function(err) {
-                   if(err){
-                         console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
-                       reject(err)
-                   }
+            await new Promise((resolve, reject) => {
+                client.quit(function (err) {
+                    if (err) {
+                        console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
+                        reject(err);
+                    }
                     console.log(`getMailList: successfully quit from pop3 server, chatId: ${chatId}`);
-                   resolve()
-               })
-           });
+                    resolve();
+                });
+            });
             return;
         }
 
         let messageCount = 0;
         for (const messageInfo of messages) {
-             console.log(`getMailList: processing message, message number: ${messageInfo.number}, chatId: ${chatId}`);
+            console.log(`getMailList: processing message, message number: ${messageInfo.number}, chatId: ${chatId}`);
             // Извлекаем сообщение по номеру
             const message = await new Promise((resolve, reject) => {
                 client.retr(messageInfo.number, function (err, data) {
-                    if (err){
+                    if (err) {
                         console.error(`getMailList: error while retrieving message, message number: ${messageInfo.number}, chatId: ${chatId}, error: ${err}`);
-                        reject(err)
+                        reject(err);
                     }
                     console.log(`getMailList: successfully retrieved message, message number: ${messageInfo.number}, chatId: ${chatId}`);
-                    resolve(data)
-                })
+                    resolve(data);
+                });
             });
-           
+
             const mailparser = new MailParser();
 
             mailparser.on('headers', (headers) => {
@@ -92,140 +98,141 @@ async function getMailList(chatId) {
                 const subject = headers.get('subject');
                 messageCount++;
                 bot.sendMessage(chatId, `Письмо ${messageCount}:\nОт: ${from}\nТема: ${subject}`);
-                 console.log(`getMailList: send message to bot, message number: ${messageInfo.number}, chatId: ${chatId}, from: ${from}, subject: ${subject}`);
+                console.log(`getMailList: send message to bot, message number: ${messageInfo.number}, chatId: ${chatId}, from: ${from}, subject: ${subject}`);
             });
-              console.log(`getMailList: mailparser write, message number: ${messageInfo.number}, chatId: ${chatId}`);
+            console.log(`getMailList: mailparser write, message number: ${messageInfo.number}, chatId: ${chatId}`);
             mailparser.write(message.raw);
             mailparser.end();
-             console.log(`getMailList: mailparser end, message number: ${messageInfo.number}, chatId: ${chatId}`);
-           
+            console.log(`getMailList: mailparser end, message number: ${messageInfo.number}, chatId: ${chatId}`);
+
         }
-          console.log(`getMailList: quitting from pop3 server, chatId: ${chatId}`);
-         await new Promise((resolve, reject) => {
-             client.quit(function(err) {
-                 if(err){
-                       console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
-                     reject(err)
-                 }
-                 console.log(`getMailList: successfully quit from pop3 server, chatId: ${chatId}`);
-                 resolve()
-             })
-         });
+        console.log(`getMailList: quitting from pop3 server, chatId: ${chatId}`);
+        await new Promise((resolve, reject) => {
+            client.quit(function (err) {
+                if (err) {
+                    console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
+                    reject(err);
+                }
+                console.log(`getMailList: successfully quit from pop3 server, chatId: ${chatId}`);
+                resolve();
+            });
+        });
     } catch (err) {
         console.error(`getMailList: catch error, chatId: ${chatId}, error: ${err}`);
         bot.sendMessage(chatId, 'Ошибка при получении списка писем.');
-         await new Promise((resolve, reject) => {
-             client.quit(function(err) {
-                 if(err){
-                       console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
-                     reject(err)
-                 }
-                 console.log(`getMailList: successfully quit from pop3 server, chatId: ${chatId}`);
-                 resolve()
-             })
-         });
+        await new Promise((resolve, reject) => {
+            client.quit(function (err) {
+                if (err) {
+                    console.error(`getMailList: error while quitting from pop3 server, chatId: ${chatId}, error: ${err}`);
+                    reject(err);
+                }
+                console.log(`getMailList: successfully quit from pop3 server, chatId: ${chatId}`);
+                resolve();
+            });
+        });
     }
     console.log(`getMailList: end, chatId: ${chatId}`);
 }
 
 // Функция для удаления письма по номеру
 async function deleteMail(number, chatId) {
-     console.log(`deleteMail: start, chatId: ${chatId}, number: ${number}`);
-      console.log(`deleteMail: using email: ${GMAIL_EMAIL}, chatId: ${chatId}, number: ${number}`);
+    console.log(`deleteMail: start, chatId: ${chatId}, number: ${number}`);
+    console.log(`deleteMail: using email: ${GMAIL_EMAIL}, chatId: ${chatId}, number: ${number}`);
     const client = new Pop3({
         host: 'pop.gmail.com',
         port: 995,
         tls: true,
         user: GMAIL_EMAIL,
         password: GMAIL_PASSWORD,
-         timeout: 10000, // устанавливаем таймаут 10 секунд
+        timeout: 10000,  // таймаут 10 секунд
+        debug: true,
     });
-     console.log(`deleteMail: pop3 client created, chatId: ${chatId}, number: ${number}`);
+    console.log(`deleteMail: pop3 client created, chatId: ${chatId}, number: ${number}`);
     try {
-         console.log(`deleteMail: connecting to pop3 server, chatId: ${chatId}, number: ${number}`);
-         await new Promise((resolve, reject) => {
-             client.connect(function (err) {
-                 if(err){
-                      console.error(`deleteMail: error connecting to pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
-                     reject(err)
-                 }
+        console.log(`deleteMail: connecting to pop3 server, chatId: ${chatId}, number: ${number}`);
+        await new Promise((resolve, reject) => {
+            client.connect(function (err) {
+                if (err) {
+                    console.error(`deleteMail: error connecting to pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
+                    reject(err);
+                }
                 console.log(`deleteMail: successfully connected to pop3 server, chatId: ${chatId}, number: ${number}`);
-                 resolve()
-             })
-         });
-         console.log(`deleteMail: fetching list of messages, chatId: ${chatId}, number: ${number}`);
-        const messages =  await new Promise((resolve, reject) => {
+                resolve();
+            });
+        });
+        console.log(`deleteMail: fetching list of messages, chatId: ${chatId}, number: ${number}`);
+        const messages = await new Promise((resolve, reject) => {
             client.list(function (err, list) {
-                 if (err) {
+                if (err) {
                     console.error(`deleteMail: error fetching list of messages, chatId: ${chatId}, number: ${number}, error: ${err}`);
                     reject(err);
                 }
                 console.log(`deleteMail: successfully fetched list of messages, chatId: ${chatId}, number: ${number}, message count: ${list.length}`);
                 resolve(list);
-            })
+            });
         });
 
         if (number > messages.length || number < 1) {
-             console.log(`deleteMail: incorrect message number, chatId: ${chatId}, number: ${number}`);
+            console.log(`deleteMail: incorrect message number, chatId: ${chatId}, number: ${number}`);
             bot.sendMessage(chatId, 'Неверный номер письма.');
-             await new Promise((resolve, reject) => {
-                 client.quit(function(err) {
-                      if(err){
-                            console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
-                         reject(err)
-                     }
-                     console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
-                     resolve()
-                 })
-             });
+            await new Promise((resolve, reject) => {
+                client.quit(function (err) {
+                    if (err) {
+                        console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
+                        reject(err);
+                    }
+                    console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
+                    resolve();
+                });
+            });
             return;
         }
 
-         console.log(`deleteMail: deleting message number: ${number}, chatId: ${chatId}`);
-         await new Promise((resolve, reject) => {
-            client.dele(number, function(err) {
-                  if(err){
-                         console.error(`deleteMail: error deleting message, message number: ${number}, chatId: ${chatId}, error: ${err}`);
-                     reject(err)
-                 }
-                  console.log(`deleteMail: successfully deleted message, message number: ${number}, chatId: ${chatId}`);
-                resolve()
-            })
+        console.log(`deleteMail: deleting message number: ${number}, chatId: ${chatId}`);
+        await new Promise((resolve, reject) => {
+            client.dele(number, function (err) {
+                if (err) {
+                    console.error(`deleteMail: error deleting message, message number: ${number}, chatId: ${chatId}, error: ${err}`);
+                    reject(err);
+                }
+                console.log(`deleteMail: successfully deleted message, message number: ${number}, chatId: ${chatId}`);
+                resolve();
+            });
         });
         bot.sendMessage(chatId, 'Письмо успешно удалено.');
-         console.log(`deleteMail: quitting from pop3 server, chatId: ${chatId}, number: ${number}`);
+        console.log(`deleteMail: quitting from pop3 server, chatId: ${chatId}, number: ${number}`);
         await new Promise((resolve, reject) => {
-            client.quit(function(err) {
-                 if(err){
-                       console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
-                     reject(err)
-                 }
-                   console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
-                resolve()
-            })
+            client.quit(function (err) {
+                if (err) {
+                    console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
+                    reject(err);
+                }
+                console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
+                resolve();
+            });
         });
     } catch (err) {
         console.error(`deleteMail: catch error, chatId: ${chatId}, number: ${number}, error: ${err}`);
         bot.sendMessage(chatId, 'Ошибка при удалении письма.');
-         await new Promise((resolve, reject) => {
-            client.quit(function(err) {
-                if(err){
-                     console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
-                    reject(err)
+        await new Promise((resolve, reject) => {
+            client.quit(function (err) {
+                if (err) {
+                    console.error(`deleteMail: error while quitting from pop3 server, chatId: ${chatId}, number: ${number}, error: ${err}`);
+                    reject(err);
                 }
-                  console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
-                resolve()
-            })
+                console.log(`deleteMail: successfully quit from pop3 server, chatId: ${chatId}, number: ${number}`);
+                resolve();
+            });
         });
     }
-     console.log(`deleteMail: end, chatId: ${chatId}, number: ${number}`);
+    console.log(`deleteMail: end, chatId: ${chatId}, number: ${number}`);
 }
 
 // Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Привет! Я бот для управления почтой Gmail.');
-     console.log(`bot: /start, chatId: ${chatId}`);
+    console.log(`bot: /start, chatId: ${chatId}`);
 });
 
 // Обработчик команды /list
@@ -233,7 +240,7 @@ bot.onText(/\/list/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Получение списка новых писем...');
     getMailList(chatId);
-      console.log(`bot: /list, chatId: ${chatId}`);
+    console.log(`bot: /list, chatId: ${chatId}`);
 });
 
 // Обработчик команды /delete
@@ -245,7 +252,7 @@ bot.onText(/\/delete (.+)/, async (msg, match) => {
         await deleteMail(number, chatId);
     } else {
         bot.sendMessage(chatId, 'Неверный формат номера письма. Используйте /delete <номер>');
-         console.log(`bot: /delete incorrect format, chatId: ${chatId}, number: ${number}`);
+        console.log(`bot: /delete incorrect format, chatId: ${chatId}, number: ${number}`);
     }
     console.log(`bot: /delete, chatId: ${chatId}, number: ${number}`);
 });
